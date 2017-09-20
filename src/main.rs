6015a0692385e6bc0fb54638ac619702b8750382
CloneRob <ropamen@gmail.com>
2017-09-20 14:13:52 +0200

    Architecture design 0.001

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,16 @@
-struct Node {}
-struct CART {}
-struct Dataset {}
-trait Feature {}
+use std::sync::Arc;
+use std::collections::HashMap;
+struct Class {}
+enum Node {
+    Intermediate{ threshold: f32},
+    Leaf(Class)
+}
+struct CART {
+    root: Box<Node>,
+}
+struct Dataset(HashMap<String, Arc<Feature>>);
+trait Feature{}
+struct Entropy {}
 
 fn main() {
     println!("Hello, world!");
